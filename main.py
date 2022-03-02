6ace00bd91efb5dca75aed5833ed2da2133b5a12
Josh Aird <75330374+LeafTickets@users.noreply.github.com>
2022-03-02 20:49:39 +0000

    Started work on game

--- main.py
+++ main.py
@@ -1,53 +1,39 @@
 import pygame, sys
 from pygame.locals import *
+import os
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((1080, 900))
-pygame.display.set_caption('Hello World!')
+DISPLAYSURF = pygame.display.set_mode((1200, 1200))
+pygame.display.set_caption('Game Window')
 purple = pygame.Color(128, 0, 128)
 black = pygame.Color(0, 0, 0)
+white = pygame.Color(0, 0, 0)
 mousex = 0
 mousey = 0
-image = pygame.image.load("Square.png")
-image2 = pygame.image.load("image2.jpg")
 moving = False
+placeholder = pygame.image.load("/home/runner/pythonGame-1/Images/Square.png")
+background = pygame.image.load("/home/runner/pythonGame-1/Images/pythonGameBackground.png")
 
 
 class card:
-  def __init__(self, nam):
-    self.name = nam
-    print(self.name,'constructed')
-  cardImage = image
-  damage = 1
-  gearCost = 0
+    def __init__(self, nam):
+        self.name = nam
+        print(self.name, 'constructed')
+
+    cardImage = placeholder
+    damage = 1
+    gearCost = 0
 
 
 card2 = card("Basic Attack")
 card2.damage = 5
-card2.cardImage = image2
-print(card2.name)
+card2.cardImage = placeholder
 
+DISPLAYSURF.blit(background, (0, 0))
 while True:
-    mouseClicked = False
     for event in pygame.event.get():
-        if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+        if event.type == QUIT or (event.type == KEYUP
+                                  and event.key == K_ESCAPE):
             pygame.quit()
             sys.exit()
-        elif event.type == MOUSEMOTION:
-            mousex, mousey = event.pos
-        elif (event.type == KEYUP and event.key == K_a) or (event.type == KEYUP and event.key == K_s):
-            mouseClicked = True
-    while mouseClicked:
-        for event in pygame.event.get():
-              if (event.type == KEYUP and event.key == K_a):
-                DISPLAYSURF.fill(black)
-                DISPLAYSURF.blit(card2.cardImage, (mousex - 200, mousey - 200))
-                mouseClicked = False
-              elif (event.type == KEYUP and event.key == K_s):
-                DISPLAYSURF.fill(black)
-                DISPLAYSURF.blit(card.cardImage, (mousex - 200, mousey - 200))
-                mouseClicked = False
-              else:  
-                pygame.display.update()
-        pygame.display.update()
     pygame.display.update()
