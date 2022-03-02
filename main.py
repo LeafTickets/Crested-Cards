0ce0596df6512b3a5619f9f0b70ce146f82ceb23
Josh Aird <75330374+LeafTickets@users.noreply.github.com>
2022-03-02 17:47:59 +0000

    Now shows two different images depending on what key is pressed

--- main.py
+++ main.py
@@ -14,12 +14,18 @@ moving = False
 
 
 class card:
-    cardImage = image
-    damage = 1
-    gearCost = 0
+  def __init__(self, nam):
+    self.name = nam
+    print(self.name,'constructed')
+  cardImage = image
+  damage = 1
+  gearCost = 0
 
 
-card.cardImage = image2
+card2 = card("Basic Attack")
+card2.damage = 5
+card2.cardImage = image2
+print(card2.name)
 
 while True:
     mouseClicked = False
@@ -29,16 +35,19 @@ while True:
             sys.exit()
         elif event.type == MOUSEMOTION:
             mousex, mousey = event.pos
-        elif event.type == MOUSEBUTTONUP:
-            mousex, mousey = event.pos
+        elif (event.type == KEYUP and event.key == K_a) or (event.type == KEYUP and event.key == K_s):
             mouseClicked = True
     while mouseClicked:
-        mousex, mousey = event.pos
-        DISPLAYSURF.fill(black)
-        DISPLAYSURF.blit(card.cardImage, (mousex - 200, mousey - 200))
         for event in pygame.event.get():
-            if event.type == MOUSEBUTTONUP:
-                mousex, mousey = event.pos
+              if (event.type == KEYUP and event.key == K_a):
+                DISPLAYSURF.fill(black)
+                DISPLAYSURF.blit(card2.cardImage, (mousex - 200, mousey - 200))
                 mouseClicked = False
+              elif (event.type == KEYUP and event.key == K_s):
+                DISPLAYSURF.fill(black)
+                DISPLAYSURF.blit(card.cardImage, (mousex - 200, mousey - 200))
+                mouseClicked = False
+              else:  
+                pygame.display.update()
         pygame.display.update()
     pygame.display.update()
