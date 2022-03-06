07274c77db840fa695d5f897a270ccf071da1179
LeafTickets <airdjosh37@gmail.com>
2022-03-05 22:11:53 -0700

    Draw cards with the keyboard press A

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ mousex = 0
 mousey = 0
 placeholder = pygame.image.load(r"C:\Users\streambox-31\PycharmProjects\pythonGame\Images\card.png")
 background = pygame.image.load(r"C:\Users\streambox-31\PycharmProjects\pythonGame\Images\pythonGameBackground.png")
+button = pygame.image.load(r"C:\Users\streambox-31\PycharmProjects\pythonGame\Images\Button.png")
 start = True
 moved = False
 turn = 0
@@ -52,32 +53,69 @@ card3 = cardGenerator("card", 6, "Attack", 8)
 card4 = cardGenerator("card", 3, "Attack", 8)
 card5 = cardGenerator("card", 3, "Attack", 8)
 card6 = cardGenerator("card", 0, "Gear", 4)
-deck = [card2, card3, card4, card5, card6]
-discard = []
-
 
 
 def drawCard(amount):
     for nums in range(0, amount):
+        if deck == []:
+            cardPop = 0
+            for cards in range(0, len(discard)):
+                poppedCard = discard.pop(cardPop)
+                deck.append(poppedCard)
+        print(deck)
         chosenCard = deck.pop(randint(0, len(deck) - 1))
         hand.append(chosenCard)
         discard.append(chosenCard)
 
+
+def cardPlace():
+    cardSelection = 0
+    cardx = 55
+    for cards in range(0, len(hand)):
+        DISPLAYSURF.blit(hand[cardSelection].cardImage, (cardx, 700))
+        cardSelection = cardSelection + 1
+        cardx = cardx + 80
+
+
+
+deck = [card2, card3, card4, card5, card6]
+discard = []
 DISPLAYSURF.blit(background, (0, 0))
 while start:
-    for event in pygame.event.get():
-        if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+    if turn == 0:
+        pygame.event.clear()
+        event = pygame.event.wait()
+        if event.type == QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == MOUSEMOTION:
-            mousex, mousey = event.pos
-    if turn == 0:
-        drawCard(2)
-        cardSelection = 0
-        cardx = 55
-        for cards in range(0, len(hand)):
-            DISPLAYSURF.blit(hand[cardSelection].cardImage, (cardx, 700))
-            cardSelection = cardSelection + 1
-            cardx = cardx + 80
-        turn = 1
-    pygame.display.update()
+        elif event.type == KEYDOWN:
+            if event.key == K_a:
+                drawCard(2)
+                cardPlace()
+                pygame.display.update()
+                pygame.event.clear()
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == KEYDOWN:
+                    if event.key == K_a:
+                        print("next turn")
+                        turn = 1
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+    elif turn == 1:
+        pygame.event.clear()
+        event = pygame.event.wait()
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == KEYDOWN:
+            if event.key == K_a:
+                print("next turn")
+                turn = 0
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+    else:
+        pygame.display.update()
