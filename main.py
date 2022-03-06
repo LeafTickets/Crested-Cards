6079f72a700303b349e7e2a59f23430a5a0d9a4e
LeafTickets <airdjosh37@gmail.com>
2022-03-06 10:45:05 -0700

    Player can now play cards

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ moved = False
 turn = 0
 hand = []
 hand2 = []
+drawn = False
 
 
 class card:
@@ -31,6 +32,7 @@ class card:
     gearCost = 0
     cardType = "Placeholder"
     moving = False
+
 
 
 card2 = card("Basic Attack")
@@ -49,15 +51,17 @@ def cardGenerator(name, damage, type, gearCost):
     return name
 
 
-card3 = cardGenerator("card", 6, "Attack", 8)
-card4 = cardGenerator("card", 3, "Attack", 8)
-card5 = cardGenerator("card", 3, "Attack", 8)
+card3 = cardGenerator("card", 6, "Attack", -8)
+card4 = cardGenerator("card", 3, "Attack", -8)
+card5 = cardGenerator("card", 3, "Attack", -8)
 card6 = cardGenerator("card", 0, "Gear", 4)
 
 
 def drawCard(amount):
     for nums in range(0, amount):
         if deck == []:
+            if discard == []:
+                return
             cardPop = 0
             for cards in range(0, len(discard)):
                 poppedCard = discard.pop(cardPop)
@@ -65,7 +69,6 @@ def drawCard(amount):
         print(deck)
         chosenCard = deck.pop(randint(0, len(deck) - 1))
         hand.append(chosenCard)
-        discard.append(chosenCard)
 
 
 def cardPlace():
@@ -77,13 +80,33 @@ def cardPlace():
         cardx = cardx + 80
 
 
+def playCard(chosenCard, gears, health):  # for target, the player is 1 while the enemy is 0
+    discardedCard = hand.pop(hand.index(chosenCard))
+    discard.append(discardedCard)
+    print(discard)
+    value1 = health
+    value2 = gears
+    if chosenCard.cardType == "Attack":
+        value1 = health - chosenCard.damage
+    if chosenCard.gearCost > 0:
+        value2 = gears + chosenCard.gearCost
+    if chosenCard.gearCost < 0:
+        if abs(chosenCard.gearCost) > gears:
+            return
+        elif abs(chosenCard.gearCost) <= gears:
+            value2 = gears + chosenCard.gearCost
+    return value1, value2
+
+
+enemyHealth = 100
+playerHealth = 100
+currentGears = 0
 deck = [card2, card3, card4, card5, card6]
 discard = []
 DISPLAYSURF.blit(background, (0, 0))
 pygame.display.update()
 while start:
     if turn == 0:
-        drawn = False
         pygame.event.clear()
         event = pygame.event.wait()
         if event.type == QUIT:
@@ -92,23 +115,31 @@ while start:
         if event.type == KEYUP:
             if event.key == K_a:
                 if not drawn:
-                    drawCard(2)
+                    drawCard(3)
                     cardPlace()
                     drawn = True
                     pygame.display.update()
-            elif event.type == KEYUP and event.key == K_e:
-                 print("CPU's turn")
-                 turn = 1
+            elif event.key == K_e:
+                print("CPU's turn")
+                turn = 1
             elif event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
+            elif event.key == K_s:
+                enemyHealth, currentGears = playCard(hand[0], currentGears, enemyHealth)
+                DISPLAYSURF.blit(background, (0, 0))
+                DISPLAYSURF.blit(hand[0].cardImage, (1100, 500))
+                cardPlace()
+                pygame.display.update()
+                print(enemyHealth, currentGears)
     elif turn == 1:
+        drawn = False
         pygame.event.clear()
         event = pygame.event.wait()
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-        elif event.type == KEYDOWN:
+        elif event.type == KEYUP:
             if event.key == K_a:
                 print("Players turn")
                 turn = 0
